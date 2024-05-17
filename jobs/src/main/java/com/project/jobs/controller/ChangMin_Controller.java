22f6cc5cdf5e9eb50d805f5d20e8e98818b45cb8
DESKTOP-4DR04SQ\GG <changminc66@gmail.com>
2024-05-17 09:37:54 +0900

    컨트롤러 수

--- jobs/src/main/java/com/project/jobs/controller/ChangMin_Controller.java
+++ jobs/src/main/java/com/project/jobs/controller/ChangMin_Controller.java
@@ -1,9 +1,18 @@
 package com.project.jobs.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.project.jobs.dto.Member;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class ChangMin_Controller {
 	
-	@RequestMapping("/signup")
-	public String sinup
+	@RequestMapping("/signUp.do")
+	public String sinup(HttpServletRequest request,Member member) {
+		
+		return "login";
+	}
 }
