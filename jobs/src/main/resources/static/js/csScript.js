1d9dd7097b75e1f88cc0b31bc56f14b82a3ede21
seowon0825 <bsw0825@naver.com>
2024-05-18 22:04:20 +0900

    고객센터 공지사항 유효성 작업 진행 중

--- jobs/src/main/resources/static/js/csScript.js
+++ jobs/src/main/resources/static/js/csScript.js
@@ -20,3 +20,20 @@ function csRegForm(){
 	
 	return true;
 };
+
+function isLoggedIn(callback) {
+	fetch('/login_status_99')
+	.then(response => response.json())
+	.then(loggedIn => {
+		if (loggedIn) {
+			window.location.href = "../write_Form_99";
+		} else {
+			if(confirm('로그인이 필요한 서비스입니다. 로그인 페이지로 이동하시겠습니까?')){
+				window.location.href="../members/loginForm";
+			}
+		}
+    	return false;
+	});
+	return false;	
+};
+
